1931e7dbe941f2a148131f882a87cd80398ca58b
Olivier Fuxet <olivier.fuxet@gmail.com>
2022-06-20 11:22:29 +0200

    feat: handle CORS for testing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,29 @@
 use crate::starknet_client::StarkNetChain;
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::{Header, Status};
+use rocket::{Request, Response};
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
 
 #[macro_use]
 extern crate rocket;
@@ -46,6 +71,15 @@ fn rocket() -> _ {
     rocket::build()
         .manage(github_client)
         .manage(starknet_client)
+        .attach(CORS)
         .mount("/", routes![rest::health_check])
-        .mount("/registrations", routes![rest::register_github_user])
+        .mount(
+            "/registrations",
+            routes![options_handler, rest::register_github_user],
+        )
+}
+
+#[route(OPTIONS, uri = "/github")]
+fn options_handler() -> Status {
+    Status::Ok
 }
